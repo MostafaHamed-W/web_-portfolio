814577da16ceaf388b41f8a7e8234a0374124a29
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-03-17 23:13:58 +0200

    Create Project Item class

---
+++ lib/views/poject/widgets/project_item.dart
@@ -0,0 +1,14 @@
+class ProjectItem {
+  final String image;
+  final String title;
+  final String discription;
+  final List<String> technologies;
+  ProjectItem({
+    required this.image,
+    required this.title,
+    required this.discription,
+    required this.technologies,
+  });
+}
+
+final kProjectItems = [ProjectItem(image: '', title: 'title', discription: '', technologies: [])];
