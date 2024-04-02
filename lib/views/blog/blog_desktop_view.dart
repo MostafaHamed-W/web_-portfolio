e30263bb13edaa56a5c3cb2707458dd482aa4510
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-04-02 23:42:16 +0200

    Add mobile blog view content

--- lib/views/blog/blog_desktop_view.dart
+++ lib/views/blog/blog_desktop_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:web_portfolio/components/desktop_view_builder.dart';
-import 'package:web_portfolio/utils/colors.dart';
 
 class BlogDesktopView extends StatelessWidget {
   const BlogDesktopView({super.key});
@@ -16,49 +15,63 @@ class BlogDesktopView extends StatelessWidget {
         Row(
           children: [
             for (int i = 0; i < 2; i++)
-              Expanded(
-                child: Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                  ),
-                  elevation: 3.0,
-                  margin: const EdgeInsets.all(8),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        decoration: const BoxDecoration(
-                          borderRadius: BorderRadius.only(topLeft: Radius.circular(15), topRight: Radius.circular(15)),
-                          color: Colors.redAccent,
-                        ),
-                        width: double.infinity,
-                        padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 8),
-                        child: Text(
-                          'How to build a portfolio',
-                          style: Theme.of(context).textTheme.headlineMedium?.copyWith(color: Colors.white),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Text('2 January 2022'),
-                            Text(
-                              'Hello world thank you' * 30,
-                              maxLines: 3,
-                            ),
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                ),
+              const Expanded(
+                child: BlogCard(),
               ),
           ],
         ),
         const SizedBox(height: 30),
       ],
+    );
+  }
+}
+
+class BlogCard extends StatelessWidget {
+  const BlogCard({
+    super.key,
+    this.isMobile = false,
+  });
+
+  final bool isMobile;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      elevation: 3.0,
+      margin: const EdgeInsets.all(8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(15), topRight: Radius.circular(15)),
+              color: Colors.redAccent,
+            ),
+            width: double.infinity,
+            padding: EdgeInsets.symmetric(vertical: isMobile ? 20 : 40, horizontal: 8),
+            child: Text(
+              'How to build a portfolio',
+              style: Theme.of(context).textTheme.headlineMedium?.copyWith(color: Colors.white),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('2 January 2022'),
+                Text(
+                  'Hello world thank you' * 30,
+                  maxLines: 3,
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/views/blog/blog_mobile_view.dart
+++ lib/views/blog/blog_mobile_view.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:web_portfolio/components/mobile_view_builder.dart';
+import 'package:web_portfolio/views/blog/blog_desktop_view.dart';
 
 class BlogMobileView extends StatelessWidget {
   const BlogMobileView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MobileViewBuilder(
+    return MobileViewBuilder(
       mainText: 'Blog',
       subText: 'My Blogs Area',
       isGraidentBackground: true,
-      children: [],
+      children: [
+        for (int i = 0; i < 2; i++) const BlogCard(isMobile: true),
+      ],
     );
   }
 }
