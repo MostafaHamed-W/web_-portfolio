7f43634917409c8abf54a1f3e8b503d9777cdfdc
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-04-08 17:38:38 +0200

    Create RSS Feed

--- lib/views/blog/blog_view.dart
+++ lib/views/blog/blog_view.dart
@@ -2,12 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:web_portfolio/components/mobile_desktop_view_builder.dart';
 import 'package:web_portfolio/views/blog/blog_desktop_view.dart';
 import 'package:web_portfolio/views/blog/blog_mobile_view.dart';
+import 'package:webfeed/webfeed.dart';
+import 'package:http/http.dart' as http;
 
 class BlogView extends StatelessWidget {
   const BlogView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MobileDesktopViewBuilder(mobileView: BlogMobileView(), desktopView: BlogDesktopView());
+    return FutureBuilder(
+        future: getArticles(),
+        builder: (context, snapshot) {
+          print(snapshot.data);
+          return const MobileDesktopViewBuilder(
+            mobileView: BlogMobileView(),
+            desktopView: BlogDesktopView(),
+          );
+        });
   }
+}
+
+getArticles() async {
+  const url = 'https://cors-anywhere.herokuapp.com/https://medium.com/feed/@muhamad_haris';
+  final uri = Uri.parse(url);
+  final response = await http.get(uri);
+  final parseResponse = RssFeed.parse(response.body);
+  return parseResponse;
 }
