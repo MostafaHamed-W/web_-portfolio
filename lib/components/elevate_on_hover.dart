d74f7d647c451c3e55719043bfb07046b9cc10c4
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-04-09 15:32:43 +0200

    Adding animation to ElevateOnHover

--- lib/components/elevate_on_hover.dart
+++ lib/components/elevate_on_hover.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
 
 class ElevateOnHover extends StatefulWidget {
-  const ElevateOnHover({super.key});
+  final Widget child;
+  const ElevateOnHover({super.key, required this.child});
 
   @override
   State<ElevateOnHover> createState() => _ElevateOnHoverState();
 }
 
 class _ElevateOnHoverState extends State<ElevateOnHover> {
+  final nonHoverTransform = Matrix4.identity()..translate(0, 0, 0);
+  final hoverTransform = Matrix4.identity()..translate(0, -10, 0);
+  bool hovering = false;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MouseRegion(
+      onEnter: (event) => mouseEnter(true),
+      onExit: (event) => mouseEnter(false),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 200),
+        transform: hovering ? hoverTransform : nonHoverTransform,
+        child: widget.child,
+      ),
+    );
+  }
+
+  void mouseEnter(bool hover) {
+    setState(() {
+      hovering = hover;
+    });
   }
 }
