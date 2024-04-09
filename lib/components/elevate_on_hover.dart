fe9517a77a35b956a15a786a5a485d4021ad344e
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-04-09 15:20:22 +0200

    Create ElevateOnHover Class

---
+++ lib/components/elevate_on_hover.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ElevateOnHover extends StatefulWidget {
+  const ElevateOnHover({super.key});
+
+  @override
+  State<ElevateOnHover> createState() => _ElevateOnHoverState();
+}
+
+class _ElevateOnHoverState extends State<ElevateOnHover> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
