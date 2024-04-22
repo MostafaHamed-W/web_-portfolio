bdff5c735d12066ce161afd8885b624abd5b99ab
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-04-22 23:10:30 +0200

    Change skills desktop view

--- lib/views/skills/skills_desktop_view.dart
+++ lib/views/skills/skills_desktop_view.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:web_portfolio/components/desktop_view_builder.dart';
 import 'package:web_portfolio/utils/constants.dart';
 import 'package:web_portfolio/views/skills/widgets/outelined_skills_container.dart';
@@ -14,22 +16,66 @@ class SkillsDesktopView extends StatelessWidget {
       subText: 'My Expertise Area',
       children: [
         const SizedBox(height: 30),
-        Column(
+        Row(
           children: [
-            for (var rowIndex = 0; rowIndex < kSkillsNames.length / 4; rowIndex++)
-              Row(
+            Expanded(
+              child: Column(
                 children: [
-                  for (var index = 0; index < kSkillsNames.length / 2; index++)
-                    Expanded(
-                      child: Padding(
-                        padding: EdgeInsets.only(left: index == 0 ? 0 : 9, bottom: 20),
-                        child: OutelinedSkillsContainer(index: index, rowIndex: rowIndex),
-                      ),
-                    )
+                  Text.rich(
+                    TextSpan(
+                      children: [
+                        const WidgetSpan(child: Icon(Icons.co_present)),
+                        const WidgetSpan(child: SizedBox(width: 10)),
+                        TextSpan(
+                          text: 'Personal Skills'.toUpperCase(),
+                          style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                                fontWeight: FontWeight.bold,
+                              ),
+                        ),
+                      ],
+                    ),
+                  )
                 ],
               ),
+            ),
+            Expanded(
+              child: Column(children: [
+                Text.rich(
+                  TextSpan(
+                    children: [
+                      const WidgetSpan(child: Icon(Icons.computer)),
+                      const WidgetSpan(child: SizedBox(width: 10)),
+                      TextSpan(
+                        text: 'Technical Skills'.toUpperCase(),
+                        style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                              fontWeight: FontWeight.bold,
+                            ),
+                      ),
+                    ],
+                  ),
+                )
+              ]),
+            )
           ],
-        )
+        ),
+        // Old skills view
+        // const SizedBox(height: 30),
+        // Column(
+        //   children: [
+        //     for (var rowIndex = 0; rowIndex < kSkillsNames.length / 4; rowIndex++)
+        //       Row(
+        //         children: [
+        //           for (var index = 0; index < kSkillsNames.length / 2; index++)
+        //             Expanded(
+        //               child: Padding(
+        //                 padding: EdgeInsets.only(left: index == 0 ? 0 : 9, bottom: 20),
+        //                 child: OutelinedSkillsContainer(index: index, rowIndex: rowIndex),
+        //               ),
+        //             )
+        //         ],
+        //       ),
+        //   ],
+        // )
       ],
     );
   }
