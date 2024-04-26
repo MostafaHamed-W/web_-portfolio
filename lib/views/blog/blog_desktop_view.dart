b329d86865a50ede2b7ff3e6e86851c2e2f6153c
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-04-26 18:02:27 +0300

    Remove Rss blogs from portfolio

--- lib/views/blog/blog_desktop_view.dart
+++ lib/views/blog/blog_desktop_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:web_portfolio/components/desktop_view_builder.dart';
-import 'package:web_portfolio/views/blog/widgets/blog_card.dart';
 import 'package:webfeed/webfeed.dart';
 
 class BlogDesktopView extends StatelessWidget {
@@ -11,23 +10,23 @@ class BlogDesktopView extends StatelessWidget {
   Widget build(BuildContext context) {
     final articles = context.watch<List<RssItem>?>();
     if (articles == null) return const CircularProgressIndicator();
-    return DesktopViewBuilder(
+    return const DesktopViewBuilder(
       mainText: 'Blog',
       subText: 'My Blogs Area',
       isGraidentBackground: true,
       children: [
-        const SizedBox(height: 30),
+        SizedBox(height: 30),
         Row(
           children: [
-            for (var article in articles)
-              Expanded(
-                child: BlogCard(
-                  article: article,
-                ),
-              ),
+            // for (var article in articles)
+            //   Expanded(
+            //     child: BlogCard(
+            //       article: article,
+            //     ),
+            //   ),
           ],
         ),
-        const SizedBox(height: 10),
+        SizedBox(height: 10),
       ],
     );
   }
--- lib/views/blog/blog_mobile_view.dart
+++ lib/views/blog/blog_mobile_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:web_portfolio/components/mobile_view_builder.dart';
-import 'package:web_portfolio/views/blog/widgets/blog_card.dart';
 import 'package:webfeed/domain/rss_item.dart';
 
 class BlogMobileView extends StatelessWidget {
@@ -11,16 +10,16 @@ class BlogMobileView extends StatelessWidget {
   Widget build(BuildContext context) {
     final articles = context.watch<List<RssItem>?>();
     if (articles == null) return const CircularProgressIndicator();
-    return MobileViewBuilder(
+    return const MobileViewBuilder(
       mainText: 'Blog',
       subText: 'My Blogs Area',
       isGraidentBackground: true,
       children: [
-        for (var article in articles)
-          BlogCard(
-            isMobile: true,
-            article: article,
-          ),
+        // for (var article in articles)
+        //   BlogCard(
+        //     isMobile: true,
+        //     article: article,
+        //   ),
       ],
     );
   }
